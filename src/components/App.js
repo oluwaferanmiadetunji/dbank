f4831084e34a7b27f4fe20fba8fd66be36d241ab
oluwaferanmiadetunji <oluwaferanmiadetunji@gmail.com>
2021-06-27 16:09:26 +0100

    finish project

--- src/components/App.js
+++ src/components/App.js
@@ -1,88 +1,195 @@
-import { Tabs, Tab } from 'react-bootstrap'
-import dBank from '../abis/dBank.json'
-import React, { Component } from 'react';
-import Token from '../abis/Token.json'
-import dbank from '../dbank.png';
-import Web3 from 'web3';
-import './App.css';
+import { Tabs, Tab } from "react-bootstrap";
+import DBank from "../abis/dBank.json";
+import React, { useState, useEffect } from "react";
+import Token from "../abis/Token.json";
+import Image from "../dbank.png";
+import Web3 from "web3";
+import "./App.css";
 
-//h0m3w0rk - add new tab to check accrued interest
+const App = props => {
+  const [web3, setWeb3] = useState("");
 
-class App extends Component {
+  const [account, setAccount] = useState("");
 
-  async componentWillMount() {
-    await this.loadBlockchainData(this.props.dispatch)
-  }
+  const [token, setToken] = useState(null);
 
-  async loadBlockchainData(dispatch) {
+  const [dbank, setDbank] = useState(null);
 
-    //check if MetaMask exists
+  const [balance, setBalance] = useState(0);
 
-      //assign to values to variables: web3, netId, accounts
+  const [dBankAddress, setDBankAddress] = useState(null);
 
-      //check if account is detected, then load balance&setStates, elsepush alert
+  const [depositAmount, setDepositAmount] = useState(0);
 
-      //in try block load contracts
-
-    //if MetaMask not exists push alert
-  }
-
-  async deposit(amount) {
-    //check if this.state.dbank is ok
-      //in try block call dBank deposit();
-  }
-
-  async withdraw(e) {
-    //prevent button from default click
-    //check if this.state.dbank is ok
-    //in try block call dBank withdraw();
-  }
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      web3: 'undefined',
-      account: '',
-      token: null,
-      dbank: null,
-      balance: 0,
-      dBankAddress: null
+  const deposit = async amount => {
+    if (dbank !== "undefined") {
+      try {
+        await dbank.methods
+          .deposit()
+          .send({ value: amount.toString(), from: account });
+      } catch (e) {
+        console.log("Error depositing", e);
+      }
     }
-  }
+  };
 
-  render() {
-    return (
-      <div className='text-monospace'>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-        <img src={dbank} className="App-logo" alt="logo" height="32"/>
+  const withdraw = async e => {
+    e.preventDefault();
+
+    if (dbank !== "undefined") {
+      try {
+        await dbank.methods.withdraw().send({ from: account });
+      } catch (e) {
+        console.log("Error withdrawing", e);
+      }
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      let web3Instance;
+
+      if (window.ethereum) {
+        web3Instance = new Web3(window.ethereum);
+        try {
+          await window.ethereum.enable();
+        } catch (e) {
+          console.log(e);
+          window.alert("Error connecting to metamask");
+        }
+      }
+      // Legacy DApp Browsers
+      else if (window.web3) {
+        web3Instance = new Web3(window.web3.currentProvider);
+      }
+      // Non-DApp Browsers
+      else {
+        window.alert("Metamask not installed");
+        const provider = new Web3.providers.HttpProvider(
+          "https://rinkeby.infura.io/v3/595676e36ea14a5a90799e19834a3892"
+        );
+        web3Instance = new Web3(provider);
+      }
+
+      setWeb3(web3Instance);
+
+      const netId = await web3Instance.eth.net.getId();
+      const accounts = await web3Instance.eth.getAccounts();
+
+      if (typeof accounts[0] !== "undefined") {
+        const balance = await web3Instance.eth.getBalance(accounts[0]);
+        setAccount(accounts[0]);
+        setBalance(balance);
+      } else {
+        window.alert("Please login with Metamask");
+      }
+
+      try {
+        setToken(
+          new web3Instance.eth.Contract(
+            Token.abi,
+            Token.networks[netId].address
+          )
+        );
+
+        setDbank(
+          new web3Instance.eth.Contract(
+            DBank.abi,
+            DBank.networks[netId].address
+          )
+        );
+
+        setDBankAddress(DBank.networks[netId].address);
+      } catch (e) {
+        console.log(e);
+        window.alert("Contract not deployed to the current network");
+      } finally {
+      }
+    })();
+  }, [dbank, props.dispatch]);
+
+  return (
+    <div className="text-monospace">
+      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <a
+          className="navbar-brand col-sm-3 col-md-2 mr-0"
+          href="http://www.dappuniversity.com/bootcamp"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={Image} className="App-logo" alt="logo" height="32" />
           <b>dBank</b>
         </a>
-        </nav>
-        <div className="container-fluid mt-5 text-center">
+      </nav>
+      <div className="container-fluid mt-5 text-center">
         <br></br>
-          <h1>{/*add welcome msg*/}</h1>
-          <h2>{/*add user address*/}</h2>
-          <br></br>
-          <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-              <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
-                {/*add Tab deposit*/}
-                {/*add Tab withdraw*/}
+        <h1>Welcome to dBank</h1>
+        <h2>{account}</h2>
+        <br></br>
+        <div className="row">
+          <main role="main" className="col-lg-12 d-flex text-center">
+            <div className="content mr-auto ml-auto">
+              <Tabs defaultActiveKey="deposit" id="uncontrolled-tab-example">
+                <Tab eventKey="deposit" title="Deposit">
+                  <br />
+                  <div>How much do you want to deposit?</div>
+                  <br />
+                  <p>
+                    Minimum amount to deposit is 0.01 ETH and you can deposit
+                    just once
+                  </p>
+                  <br />
+                  <form
+                    onSubmit={e => {
+                      e.preventDefault();
+                      deposit(depositAmount * 10 ** 8);
+                    }}
+                  >
+                    <div className="form-group mr-sm-2">
+                      <br />
+                      <input
+                        id="depositAmount"
+                        step="0.01"
+                        type="number"
+                        min="0.01"
+                        className="form-control form-control-md"
+                        placeholder="Enter Amount to deposit ..."
+                        required
+                        value={depositAmount}
+                        onChange={event => {
+                          setDepositAmount(event.target.value);
+                        }}
+                      />
+
+                      <button type="submit" className="btn btn-primary mt-4">
+                        Deposit
+                      </button>
+                    </div>
+                  </form>
+                </Tab>
+
+                <Tab eventKey="withdraw" title="Withdraw">
+                  <br />
+                  <div>How much do you want to withdraw and take interest?</div>
+                  <div>
+                    <button
+                      type="submit"
+                      className="btn btn-primary mt-4"
+                      onClick={async e => {
+                        await withdraw(e);
+                      }}
+                    >
+                      Withdraw
+                    </button>
+                  </div>
+                </Tab>
               </Tabs>
-              </div>
-            </main>
-          </div>
+            </div>
+          </main>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
